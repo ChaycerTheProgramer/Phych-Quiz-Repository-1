1c80225e996f28687d0d7dfc8f56a4680abf318a
ChaycerTheProgramer <89816111+ChaycerTheProgramer@users.noreply.github.com>
2022-03-24 23:55:01 -0600

    wee

--- Connect_4Game.js
+++ Connect_4Game.js
@@ -1,6 +1,6 @@
 var EatDickBtn = document.getElementById("EatDickBtn");
 var msg = document.getElementById("msg");
 function tits(){
-	msg.style.display = initial;
-	EatDickBtn.style.display = none;
+	msg.style.display = "initial";
+	EatDickBtn.style.display = "none";
 };
