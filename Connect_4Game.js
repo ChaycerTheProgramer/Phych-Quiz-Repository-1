9283a8b2b0f2cdfa03f7995134f614b974806f03
ChaycerTheProgramer <89816111+ChaycerTheProgramer@users.noreply.github.com>
2022-03-24 23:11:27 -0600

    Testing Repository

--- Connect_4Game.js
+++ Connect_4Game.js
@@ -1,1 +1,6 @@
-
+var EatDickBtn = document.getElementById("EatDickBtn");
+var msg = document.getElementById("msg");
+function (){
+	msg.style.display = initial;
+	EatDickBtn.style.display = none;
+}
