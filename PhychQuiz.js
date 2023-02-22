cb183a5f092a84f0a40ab9f69633618120ccc906
ChaycerTheProgramer <89816111+ChaycerTheProgramer@users.noreply.github.com>
2023-02-22 16:30:56 -0700

    Update PhychQuiz.js

--- PhychQuiz.js
+++ PhychQuiz.js
@@ -5,7 +5,7 @@ var MainTitleScreen = document.getElementById("MainTitleScreen");
 var MainQuiz = document.getElementById("MainQuiz");
 //Function Variables
 var QuestNum = 0;
-var AnsIndex[];
+
 //Eventlisteners
 StartBtn.addEventListener("click", StartQuiz);
 
@@ -19,4 +19,3 @@ function StartQuiz() {
 //Question Index
 
 //Awnser Index
-var Ans[] = "Ans1","Ans2","Ans3","Ans4","Ans5","Ans6";
